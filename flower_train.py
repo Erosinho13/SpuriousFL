07279cce3f81ebd31e119f93510ac4fb14ccf43d
Eros Fanì <eros.fani@gmail.com>
2024-05-27 13:48:59 +0100

    added wandb entity

--- flower_train.py
+++ flower_train.py
@@ -44,6 +44,7 @@ def train():
         import wandb
         wandb.init(
             project="spurious_FL",
+            entity="predictive-analytics-lab",
             config=conf,
             id=conf["exp_id"],
             job_type="train",
