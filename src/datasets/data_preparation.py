0cfad772ca1721ea7e74976430003e4e3587ebf0
Gergely Nemeth <neged.ng@gmail.com>
2024-05-30 16:44:33 +0200

    update valsplit to match new SubsetDataset format

--- src/datasets/data_preparation.py
+++ src/datasets/data_preparation.py
@@ -1,12 +1,10 @@
 import torchvision
-from torch.utils.data import random_split, Dataset
 import torch
-from PIL import Image
 import copy
 import numpy as np
 
 from src.datasets.cifar import CIFAR10, data_transforms_cifar10, cifar_split_data
-
+from src.datasets.subset import SubsetDataset
 
 def load_data(dataset_mode="CIFAR10", val_split=False, val_ratio=0.2, conf={}):
     """Load datasets into dataset object"""
@@ -28,16 +26,14 @@ def load_data(dataset_mode="CIFAR10", val_split=False, val_ratio=0.2, conf={}):
         )
 
         if val_split:
-            raise ValueError("Code needs update")
+            rand_ids = np.random.default_rng(seed=42).permutation(len(trainset))
             len_val = int(len(trainset) * val_ratio)
             len_train = len(trainset) - len_val
-            trainset, valset = random_split(
-                trainset,
-                [len_train, len_val],
-                torch.Generator().manual_seed(conf["seed"]),
-            )
-            trainset = SubsetDataset(trainset)
-            valset = SubsetDataset(valset)
+            train_ids = rand_ids[:len_train]
+            val_ids = rand_ids[val_ids:]
+            trainset_reduced = SubsetDataset(trainset, train_ids)
+            valset = SubsetDataset(trainset, val_ids)
+            return trainset_reduced, valset, testset
         else:
             valset = copy.deepcopy(testset)
         return trainset, valset, testset
@@ -67,55 +63,3 @@ def split_data(ds, conf):
         return ds_split
     raise NotImplementedError(f"Dataset split for dataset {conf['dataset']} not recognized")
 
-
-
-
-def get_np_from_dataset(dataset):
-    return np.array(dataset.data), np.array(dataset.targets)
-
-
-def get_np_from_dataloader(dataloader):
-    image_batches = []
-    label_batches = []
-    for images, labels in dataloader:
-        image_batches.append(images.detach().numpy())
-        label_batches.append(labels.detach().numpy())
-    numpy_labels = np.concatenate(label_batches, axis=0)
-    numpy_images = np.concatenate(image_batches, axis=0)
-    numpy_images = np.transpose(numpy_images, (0, 2, 3, 1))
-    return numpy_images, numpy_labels
-
-
-def get_np_from_ds(data):
-    #!TODO rework this
-    #!TODO not working for Subset
-    if isinstance(data, torch.utils.data.DataLoader):
-        return get_np_from_dataloader(data)
-    return get_np_from_dataset(data)
-
-
-class CustomImageDataset(torch.utils.data.Dataset):
-    def __init__(self, data, transform=None, target_transform=None):
-        self.data = data[0]
-        self.targets = data[1]
-        self.transform = transform
-        self.target_transform = target_transform
-
-    def __len__(self):
-        return len(self.data)
-
-    def __getitem__(self, idx):
-        arr = self.data[idx]
-        if arr.dtype == np.float32 or arr.dtype == np.float64:
-            arr = (arr * 255).astype(np.uint8)
-        image = Image.fromarray(arr)
-        label = self.targets[idx]
-        if self.transform:
-            image = self.transform(image)
-        if self.target_transform:
-            label = self.target_transform(label)
-        return image, label
-
-
-def get_ds_from_np(data):
-    return CustomImageDataset(data, transform=None, target_transform=None)
