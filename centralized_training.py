d446b81c4f703650e0f592ef79e7b7b5ae20f26d
Gergely Nemeth <neged.ng@gmail.com>
2024-09-11 11:01:55 +0200

    more log to find error in CRT

--- centralized_training.py
+++ centralized_training.py
@@ -166,6 +166,8 @@ def train(conf, conf_path=None):
         train_loader = WeightedDataLoader(dataset=train_ds, weights=None,
                                           batch_size=conf['batch_size'], shuffle=True)
     print("Dataset size:", len(train_ds))
+    if is_two_stage_optimizer(conf):
+        print("Trainable parameters:", model_utils.count_params(model, only_trainable=True))
     opt = get_subpop_optimizer(model, train_ds, conf)
     for epoch in range(conf['epochs']):
 
